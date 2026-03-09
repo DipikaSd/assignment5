291a19ccca570162fc242583b407f4466d7f4e43
Dipika Sutradhar <dipikasutradhar37@gmail.com>
2026-03-09 19:42:11 +0600

    fetch issues from Api

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
 
 const container = document.getElementById("issuesContainer");
 
-async function loadIssues(type){
+async function loadIssues(type="all"){
 
-container.innerHTML = "Loading...";
+setActiveTab(type);
 
 const res = await fetch(API);
-const data = await res.json();
+const result = await res.json();
 
-let issues = data.data;
+let issues = result.data;
 
 if(type === "open"){
 issues = issues.filter(i => i.status === "open");
@@ -21,35 +21,64 @@ issues = issues.filter(i => i.status === "closed");
 
 displayIssues(issues);
 
+document.getElementById("totalIssues").innerText = issues.length;
+
+}
+function setActiveTab(type){
+
+const buttons = document.querySelectorAll(".tabBtn");
+
+buttons.forEach(btn=>{
+btn.classList.remove("bg-purple-600","text-white");
+btn.classList.add("border");
+});
+
+if(type === "all"){
+document.getElementById("allBtn").classList.add("bg-purple-600","text-white");
+}
+
+if(type === "open"){
+document.getElementById("openBtn").classList.add("bg-purple-600","text-white");
+}
+
+if(type === "closed"){
+document.getElementById("closedBtn").classList.add("bg-purple-600","text-white");
+}
+
 }
 
 
 
 function displayIssues(issues){
 
+const container = document.getElementById("issuesContainer");
 container.innerHTML = "";
+
+document.getElementById("totalIssues").innerText = issues.length;
 
 issues.forEach(issue => {
 
-const borderColor =
+const border =
 issue.status === "open"
 ? "border-t-4 border-green-500"
 : "border-t-4 border-purple-500";
 
 const card = document.createElement("div");
 
-card.className = `bg-white p-4 rounded shadow ${borderColor}`;
+card.className = `bg-white p-4 rounded shadow cursor-pointer ${border}`;
 
 card.innerHTML = `
-<h3 class="font-bold">${issue.title}</h3>
+
+<h3 class="font-bold mb-2">${issue.title}</h3>
 
 <p class="text-sm text-gray-500 mb-2">
 ${issue.description}
 </p>
 
-<p>Status: ${issue.status}</p>
-<p>Author: ${issue.author}</p>
-<p>Priority: ${issue.priority}</p>
+<p class="text-sm">Status: ${issue.status}</p>
+<p class="text-sm">Author: ${issue.author}</p>
+<p class="text-sm">Priority: ${issue.priority}</p>
+
 `;
 
 card.onclick = () => openModal(issue.id);
@@ -59,6 +88,7 @@ container.appendChild(card);
 });
 
 }
+
 
 async function openModal(id){
 
@@ -83,6 +113,7 @@ function closeModal(){
 document.getElementById("modal").classList.add("hidden");
 }
 
+
 async function searchIssue(){
 
 const text = document.getElementById("searchInput").value;
@@ -98,23 +129,6 @@ displayIssues(data.data);
 }
 
 loadIssues("all");
-function setActiveTab(type){
 
-document.querySelectorAll(".tabBtn").forEach(btn=>{
-btn.classList.remove("bg-purple-600","text-white");
-});
 
-if(type === "all"){
-document.getElementById("allBtn").classList.add("bg-purple-600","text-white");
-}
 
-if(type === "open"){
-document.getElementById("openBtn").classList.add("bg-purple-600","text-white");
-}
-
-if(type === "closed"){
-document.getElementById("closedBtn").classList.add("bg-purple-600","text-white");
-}
-
-}
-loadIssues("all");
